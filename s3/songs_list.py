3515102a0f7d4c11f839408a98d364bbdcd73ff2
Divya <97727498+div9556@users.noreply.github.com>
2022-03-15 21:57:05 -0700

    Update songs_list.py

--- s3/songs_list.py
+++ s3/songs_list.py
@@ -35,6 +35,48 @@ def load_db():
         next(rdr)  # Skip header line
         for Song_name, Length_of_the_music, Artist, Producers, Language, Rating_of_the_song, Released_Year, Id, genre in rdr:
             database[Id] = (Song_name, Length_of_the_music, Artist, Producers, Language, Rating_of_the_song, Released_Year, genre)
+            
+            
+@bp.route('/deleteall', methods=['DELETE'])
+def deleteall_songs():
+    global database
+    id_list=[]
+    for Id, value in database.items():
+        id_list.append(Id)
+    for i in id_list:
+        del database[i]
+    return {}
+
+@bp.route('/update/<Id1>', methods=['PUT'])
+def update_song(Id1):
+    global database
+    if Id1 in database:
+        try:
+            content = request.get_json()
+            Artist = content['Artist']
+            Song_name= content['Song_name']
+            Length_of_the_music= content['Length_of_the_music']
+            Producers= content['Producers']
+            Language= content['Language']
+            Rating_of_the_song= content['Rating_of_the_song']
+            Released_Year= content['Released_Year']
+            genre= content['genre']
+        except Exception:
+            return app.make_response(
+            ({"Message": "Error reading arguments"}, 400)
+            )
+        Id = Id1
+        database[Id] = (Song_name, Length_of_the_music, Artist, Producers, Language, Rating_of_the_song, Released_Year,genre)
+        response = {
+            "Id": Id
+        }
+    else:
+        response = {
+            "Count": 0,
+            "Items": []
+        }
+        return app.make_response((response, 404))
+    return response
 
 
 @bp.route('/health')
